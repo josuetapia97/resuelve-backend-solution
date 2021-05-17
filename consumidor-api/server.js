8872f24e178b138b6475fa8247d947eaccc39cce
Josue Tapia <josue.tapia.comp@gmail.com>
2021-05-16 20:40:29 -0500

    version funcional del backend

--- consumidor-api/server.js
+++ consumidor-api/server.js
@@ -1,23 +1,52 @@
 const express = require('express');
-const path = require('path');
 const fetch = require('node-fetch');
-const app = express()
+const app = express();
 const port = 3001;
-//console.log(jsonFile);
+
+const fetchData = async (url) =>{
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return error;
+    }
+}
+const fetchAllData = async (json) => {
+    const jugadores = [...json];
+    for(const jugador of jugadores) {
+        try {
+            jugador.nombre += (jugador.apellido)?
+                " "+jugador.apellido:""
+            delete jugador.apellido;
+            const nivel = await fetchData(jugador.nivel);
+            jugador.nivel = nivel.nivel;
+            jugador.goles_minimos = nivel.goles_mes;
+            const equipo = await fetchData(jugador.equipo);
+            jugador.sueldo_completo = null;
+            jugador.equipo = equipo.nombre;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return {jugadores};
+}
 const getPlayersFromApi = async () =>{
     try {
         const res = await fetch('http://localhost:8000/jugadores/')
-        const json_resp = await res.json()
-        return json_resp;
+        const jsonResp = await res.json()
+        const jsonRespAllData = await fetchAllData(jsonResp);
+        return jsonRespAllData;
     } catch (error) {
         console.log(error);
     }
 }
 
+
 app.get(
-    '/',
+    '/jugadores',
     async function(req,res){
-        const json = await getPlayersFromApi(); 
+        const json = await getPlayersFromApi();
         res.send(json);
     }
 );
